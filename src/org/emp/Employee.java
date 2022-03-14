57badd1787b01f924736555f09b985cf535054b7
MuraliKavi <pnbabu71@gmail.com>
2022-03-14 12:06:22 +0530

    file add

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,34 @@
+package org.emp;
+
+public class Employee {
+	
+private void empId() {
+System.out.println("4554");
+}
+
+private void empName() {
+System.out.println("Murali");
+}
+private void empDob() {
+System.out.println("01/07/993");
+}
+private void empPhone() {
+System.out.println("8220442281");
+}
+private void empEmail() {
+System.out.println("anmurali.eee@gmail.com");
+}
+private void empAddress() {
+System.out.println("chennai");
+}
+public static void main(String[] args) {
+	
+	Employee t =new Employee();
+	t.empId();
+	t.empName();
+	t.empDob();
+	t.empPhone();
+	t.empEmail();
+	t.empAddress();
+}
+}
